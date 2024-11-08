061638adb8f8883cd94633abea878adf72d40460
Jonathan Catota <jonathan_catota77@hotmail.com>
2024-11-08 16:07:58 -0500

    Tareas del paso 9 completadas y se realizo un fix Eslint

--- games/prime.js
+++ games/prime.js
@@ -2,17 +2,16 @@
 import getRandomInt from '../utils/random-number.js';
 // Función para verificar si un número es primo
 const esPrimo = (numero) => {
-    if (numero <= 1) return false; 
-    for (let i = 2; i <= Math.sqrt(numero); i++) {
-        if (numero % i === 0) return false; // Si el número es divisible por algún número entre 2 y su raíz cuadrada
-    }
-    return true;
-}
+  if (numero <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(numero); i++) {
+    if (numero % i === 0) return false; // Si el número es divisible por algún número entre 2 y su raíz cuadrada
+  }
+  return true;
+};
 const gamePrimeLogic = () => {
+  const question = getRandomInt(1, 1000);
+  const correctAnswer = esPrimo(question) ? 'yes' : 'no';
 
-    const question = getRandomInt(1, 1000);
-    const correctAnswer = esPrimo(question) ? "yes" : "no";
-    
-    return { question, correctAnswer };
-}
+  return { question, correctAnswer };
+};
 export default gamePrimeLogic;
